fe4ad040169d2a10e32df2c8f125405f6efa3470
KristapsBB <mihailpetrenko66@gmail.com>
2024-05-02 10:34:47 +0500

    Add styles for the error page

--- Views/error.php
+++ Views/error.php
@@ -4,12 +4,12 @@
  * @var DiplomaProject\Core\Modules\Viewer $this
  */
 
-$this->page_params['title'] = 'Errors page';
+$this->page_params['title'] = 'Error ' . $this->code;
 ?>
 
-<div class="error-page">
+<div class="error-page error-block">
     <h1>Error <?php echo $this->code; ?></h1>
-    <div class="error">
+    <div class="error-block__message">
         <?php echo $this->params['error']; ?>
     </div>
 </div>
