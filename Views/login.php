9de7fccb6987bd63af45bca14634ae461334302f
KristapsBB <mihailpetrenko66@gmail.com>
2024-05-02 10:30:01 +0500

    Add styles for the login page

--- Views/login.php
+++ Views/login.php
@@ -9,22 +9,28 @@ use DiplomaProject\Core\Modules\Viewer;
 $this->page_params['title'] = 'Login page';
 ?>
 
-<div class="login-page">
+<div class="login-page login-block">
     <h1>Login Page</h1>
-    <div class="error">
-        <?php if (!empty($this->params['error'])) {
-            echo 'Error: ' . $this->params['error'];
-        } ?>
-    </div>
-    <form action="/login" method="POST">
-        <div>
-            <label for="login">login: </label>
-            <input type="text" name="login-form[login]" id="login">
+    <form action="/login" method="POST" class="login-form">
+        <div class="login-form__error ">
+            <?php if (!empty($this->params['error'])) {
+                echo 'Error: ' . $this->params['error'];
+            } ?>
         </div>
-        <div>
-            <label for="password">password: </label>
-            <input type="password" name="login-form[password]" id="password">
+        <div class="login-form__input-wrapper">
+            <label for="login" class="login-form__field-label">
+                login:
+            </label>
+            <input type="text" name="login-form[login]" id="login" class="login-form__field-input">
         </div>
-        <button type="submit">login</button>
+        <div class="login-form__input-wrapper">
+            <label for="password" class="login-form__field-label">
+                password:
+            </label>
+            <input type="password" name="login-form[password]" id="password" class="login-form__field-input">
+        </div>
+        <div class=" login-form__submit-wrapper">
+            <button type="submit" class="login-form__submit">login</button>
+        </div>
     </form>
 </div>
